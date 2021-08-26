ef7a23579ae4b78f2d77405026c73971796dc7a2
Roshini752 <88433698+Roshini752@users.noreply.github.com>
2021-08-26 19:54:30 +0530

    Update count_1s_0s.c

--- count_1s_0s.c
+++ count_1s_0s.c
@@ -1,12 +1,9 @@
 //W.A.P to count total 1's and 0's in a decimal number
 #include<stdio.h>
-int main()
+int count(int num,int count0,int count1)
 {
-	int i,num;
-	printf("enter the num value\n");
-	scanf("%d",&num);  // 16--> 10000
-	int count1=0,count0=0;
-	for(i=0;i<32;i++)
+    int i;
+    for(i=0;i<32;i++)
 	{
 	if((num>>i)&1)  
 	{
@@ -15,8 +12,17 @@ int main()
 	else
 	{
 	count0++;
-	} 
+	}
 	}
 	printf("ones=%d\n",count1);
 	printf("zeros=%d\n",count0);
+}
+int main()
+{
+	int i,num;
+    int count0=0,count1=0;
+	printf("enter the num value\n");
+	scanf("%d",&num);  // 16--> 10000
+	count(num,count0,count1);
+	
 }
